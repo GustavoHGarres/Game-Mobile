1aea854a5cfa204ee9576abdb1270c3a685c337a
GustavoHGarres <ghgarres@gmail.com>
2025-03-14 21:48:20 -0300

    Modulo 20 - Mobile, primeiro commit.

---
+++ Assets/Scripts/ItemCollactableBase.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class ItemCollactableBase : MonoBehaviour
+{
+    public string comparetag = "Player";
+    public ParticleSystem particleSystem;
+    public AudioSource audioSource;
+    public float timeToHide = 3;
+    public GameObject graphicItem;
+
+    public void Awake()
+    {
+        if (particleSystem != null) particleSystem.transform.SetParent(null);
+    }
+        
+        private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.transform.CompareTag(comparetag))
+       {
+         Collect();
+       }
+    }
+
+    protected virtual void Collect()
+    {
+        if (graphicItem != null) graphicItem.SetActive(false);
+        Invoke("HideObject", timeToHide);
+        OnCollect();        
+    }  
+
+    private void HideObject()
+    {
+         gameObject.SetActive(false);
+    }
+
+    protected virtual void OnCollect()
+    {
+        if(particleSystem != null) particleSystem.Play();
+        if (audioSource != null) audioSource.Play();
+    }
+  
+}
+
+
+
