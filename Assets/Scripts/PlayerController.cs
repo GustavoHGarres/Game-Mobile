2cfbd2fe6cd1bd9660fc5b5b99738ec7ea1cbd97
GustavoHGarres <ghgarres@gmail.com>
2025-03-16 02:03:15 -0300

    Modulo 20 - Movimento do Player, coleta de itens, inicio e retorno do jogo.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    //Publics
+    [Header("Lerp")]
+    public Transform target;
+    public float lerpSpeed = 1f;
+
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
+    public string tagToChecKEnemy = "Enemy";
+    public string tagToChecKEndLine = "EndLine";
+
+    public GameObject endScreen;
+
+    //Privates
+    private bool _canRun;
+    private Vector3 _pos;
+
+   
+       
+      void Update()
     {
-        
+        if(!_canRun) return;
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        transform.Translate(transform.forward * speed * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        if (collision.transform.tag == tagToChecKEnemy)
+        {
+            EndGame();
+        }      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == tagToChecKEndLine)
+        {
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+         _canRun = false;
+         endScreen.SetActive(true);
+    }
+
+     public void StartRun()
+    {
+        _canRun = true;
     }
 }
