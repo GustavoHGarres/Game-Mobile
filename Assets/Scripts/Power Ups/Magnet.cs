e2c7594880928ebc3a4cd75466783ea184ca2e1e
GustavoHGarres <ghgarres@gmail.com>
2025-04-21 21:48:19 -0300

    Tarefa do módulo 25

--- Assets/Scripts/Power Ups/Magnet.cs
+++ Assets/Scripts/Power Ups/Magnet.cs
@@ -8,6 +8,10 @@ public class Magnet : MonoBehaviour
 
     public GameObject coinDetectorObj;
 
+    //Particles
+    public string comparetag = "Player";
+    public ParticleSystem particleSystem;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +25,7 @@ public class Magnet : MonoBehaviour
         {
             StartCoroutine(ActivateCoin());
             Destroy(transform.GetChild(0).gameObject);
-            
+            MagnetParticle();
         }
     }
 
@@ -30,5 +34,12 @@ public class Magnet : MonoBehaviour
         coinDetectorObj.SetActive(true);
         yield return new WaitForSeconds(6f);
         coinDetectorObj.SetActive(false);
+    }
+
+    //Particles
+
+     public void MagnetParticle()
+    {
+        if(particleSystem != null) particleSystem.Play();
     }
 }
